f73b50ea0b3234f9b62e4449321722ddaae86f86
Diego Neri Felix <Diego Neri>
2025-05-19 09:26:03 -0300

    Finaliza programa

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int numeroDigitado;
+int numeroExibido = 0;
+
+Console.Write("Números pares entre 0 e ? ");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+while (numeroExibido <= numeroDigitado)
+{
+    Console.Write($"{numeroExibido} ");
+    numeroExibido += 2;
+}
