207abad5e4496e88c2051d72d17e82c7af8dcf32
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-06-06 20:08:36 -0300

    Adiciona pausa ao finalizar

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿int numeroDigitado;
 int numeroExibido = 0;
 
-Console.Write("Números pares entre 0 e ? ");
+Console.Write("-- Números pares entre 0 e ? ");
 numeroDigitado = Convert.ToInt32(Console.ReadLine());
 
 while (numeroExibido <= numeroDigitado)
@@ -9,3 +9,6 @@ while (numeroExibido <= numeroDigitado)
     Console.Write($"{numeroExibido} ");
     numeroExibido += 2;
 }
+
+Console.WriteLine("\n\nPressione uma tecla para finalizar...");
+Console.ReadKey(true);
